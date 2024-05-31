22df7b8776d304840a684e264ddaebb5d8abaeaf
mobyitalong <165924835+Mobyitalong@users.noreply.github.com>
2024-05-31 23:59:33 +0000

    Routing requests

--- src/app/main.go
+++ src/app/main.go
@@ -11,10 +11,24 @@ func home(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("Hello from the app"))
 }
 
+func snippetView(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Display a specific snippet..."))
+}
+
+func snippetCreate(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Display a form to create a new snippet..."))
+}
+
 func main() {
 	// A serverMux in Go terminology is considered as the router - stores mapping between URL routing patterns and corresponding handlers
 	mux := http.NewServeMux()
-	mux.HandleFunc("/", home)
+
+	// Restrict subtree path patterns using the `{$}` syntax after the trailing slash
+	// NB: This will only work on paths ending with a trailing slash - otherwise may cause a runtime panic!
+	mux.HandleFunc("/{$}", home)
+
+	mux.HandleFunc("/snippet/view", snippetView)
+	mux.HandleFunc("/snippet/create", snippetCreate)
 
 	const addr string = ":4000"
 
