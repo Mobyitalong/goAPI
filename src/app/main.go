4ebf579f88f82a1c855a0346a70caff84cc0bb8e
mobyitalong <165924835+Mobyitalong@users.noreply.github.com>
2024-05-31 23:48:10 +0000

    Web application basics

--- src/app/main.go
+++ src/app/main.go
@@ -1,7 +1,25 @@
 package main
 
-import "fmt"
+import (
+	"log"
+	"net/http"
+)
+
+// Define a home handler function to write a byte slice as response body.
+// NB: Handlers are responsible for executing application logic and writing HTTP response headers + bodies
+func home(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Hello from the app"))
+}
 
 func main() {
-	fmt.Println("Hello world!")
+	// A serverMux in Go terminology is considered as the router - stores mapping between URL routing patterns and corresponding handlers
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", home)
+
+	const addr string = ":4000"
+
+	log.Print("Staring server on " + addr)
+
+	err := http.ListenAndServe(addr, mux)
+	log.Fatal(err)
 }
