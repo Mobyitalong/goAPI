86e2d997f46e290ffdb115f2e1bf24f94f2f0090
mobyitalong <165924835+Mobyitalong@users.noreply.github.com>
2024-06-01 00:32:04 +0000

    Wildcard route patterns

--- src/app/main.go
+++ src/app/main.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
+	"strconv"
 )
 
 // Define a home handler function to write a byte slice as response body.
@@ -12,7 +14,14 @@ func home(w http.ResponseWriter, r *http.Request) {
 }
 
 func snippetView(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Display a specific snippet..."))
+	id, err := strconv.Atoi(r.PathValue("id"))
+	if err != nil || id < 1 {
+		http.NotFound(w, r)
+		return
+	}
+
+	msg := fmt.Sprintf("Display a specific snippet with ID %d...", id)
+	w.Write([]byte(msg))
 }
 
 func snippetCreate(w http.ResponseWriter, r *http.Request) {
@@ -27,7 +36,7 @@ func main() {
 	// NB: This will only work on paths ending with a trailing slash - otherwise may cause a runtime panic!
 	mux.HandleFunc("/{$}", home)
 
-	mux.HandleFunc("/snippet/view", snippetView)
+	mux.HandleFunc("/snippet/view/{id}", snippetView)
 	mux.HandleFunc("/snippet/create", snippetCreate)
 
 	const addr string = ":4000"
